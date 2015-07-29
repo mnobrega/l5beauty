d46372ae9aeb00e092a87b9dbe3c06d78ceab4c1
Marcio Nobrega <marcio.nobrega@sapo.pt>
2015-07-29 01:14:14 +0100

    Started adding admin area. Chapter 8.

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,24 @@
+<html lang="pt">
+    <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+            <meta name="viewport" content="with-device-width, initial-scale=1">
+
+            <title>{{ config('blog.title') }} Admi </title>
+
+            <link href="//maxcdn.boostrappcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
+            @yield('styles')
+    </head>
+
+    <body>
+        {{ -- Navigation Bar --}}
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu">
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
