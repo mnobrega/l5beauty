36b7508d6a690178bbf2cf6dde2b832c56bdde7b
Marcio Nobrega <marcio.nobrega@sapo.pt>
2015-07-30 01:41:53 +0100

    Finished Chapter 8 - admin area. Added Gulp and Bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,27 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
 var elixir = require('laravel-elixir');
+
+/**
+ * Copy any needed files.
+ *
+ * Do a 'gulp copyfiles' after bower updates
+ */
+
+gulp.task("copyfiles", function() {
+    gulp.src("vendor/bower_dl/jquery/dist/jquery.js").pipe(gulp.dest("resources/assets/js"));
+    gulp.src("vendor/bower_dl/bootstrap/less/**").pipe(gulp.dest("resources/assets/less/bootstrap"));
+    gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js").pipe(gulp.dest("resources/assets/js"));
+    gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**").pipe(gulp.dest("public/assets/fonts"));
+    gulp.src("vendor/bower_dl/fontawesome/less/**").pipe(gulp.dest("resources/assets/less/fontawesome"));
+    gulp.src("vendor/bower_dl/fontawesome/fonts/**").pipe(gulp.dest("public/assets/fonts"));
+
+    //copy datatables
+    var dtDir = 'vendor/bower_dl/datatables-plugins/integration/';
+    gulp.src("vendor/bower_dl/datatables/media/js/jquery.dataTables.js").pipe(gulp.dest("resources/assets/js/"));
+    gulp.src(dtDir+'bootstrap/3/dataTables.bootstrap.css').pipe(rename('dataTables.bootstrap.less')).pipe(gulp.dest("resources/assets/less/others/"));
+    gulp.src(dtDir+'bootstrap/3/dataTables.bootstrap.js').pipe(gulp.dest("resources/assets/js/"));
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +35,14 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.scripts([
+            'js/jquery.js',
+            'js/bootstrap.js',
+            'js/jquery.dataTables.js',
+            'js/dataTables.bootstrap.js'
+    ],
+    'public/assets/js/admin.js',
+    'resources/assets');
+
+    mix.less('admin.less','public/assets/css/admin.css');
 });
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -4,21 +4,36 @@
             <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
             <meta name="viewport" content="with-device-width, initial-scale=1">
 
-            <title>{{ config('blog.title') }} Admi </title>
+            <title>{{ config('blog.title') }} Admin </title>
 
-            <link href="//maxcdn.boostrappcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+            <link href="/assets/css/admin.css" rel="stylesheet">
 
             @yield('styles')
     </head>
 
     <body>
-        {{ -- Navigation Bar --}}
+        {{-- Navigation Bar --}}
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">{{ config('blog.title') }} Admin</a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    @include('admin.partials.navbar')
                 </div>
             </div>
         </nav>
+
+        @yield('content')
+
+        <script src="/assets/js/admin.js"></script>
+
+        @yield('scripts')
     </body>
 </html>
